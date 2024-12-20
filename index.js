660bd1b68333eb037eb45e6d2b35abc630be12a8
Raj Yadav <94789028+rajyadav282@users.noreply.github.com>
2024-12-20 21:20:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const users = [];
 
 
 app.post("/signup" , function(req,res){
-    const username = req.body.username;
+    const username = req.body.username;    
     const password = req.body.password;
 
     users.push({
